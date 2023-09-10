24bac59c0ea6e61cc2329f6f70ad8665dec466b4
Juan Barrera <jcbp1999@gmail.com>
2023-09-10 14:18:40 -0500

    birthday cards!

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-import time
-import turtle as t
-from playing_turtle import CrossingTurtle
-from car_manager import CarManager
-from score import Score
-
-screen = t.Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-
-screen.listen()
-
-all_cars = CarManager()
-juan = CrossingTurtle()
-score = Score()
-screen.update()
-
-# letting the player move the turtle
-screen.onkeypress(key="Up", fun=juan.move_up)
-screen.onkeypress(key="Down", fun=juan.move_down)
-screen.onkeypress(key="Left", fun=juan.move_left)
-screen.onkeypress(key="Right", fun=juan.move_right)
-
-game_over = False
-while not game_over:
-    time.sleep(0.1)
-    all_cars.move()
-    for car in all_cars.cars:
-        if car.xcor() < -300:
-            all_cars.restart_car(car)
-        x_distance = juan.xcor() - car.xcor()
-        if car.distance(juan) < 20:
-            game_over = True
-            score.game_over()
-    if juan.ycor() > 260:
-        score.next_level()
-        juan.next_level()
-        all_cars.next_level()
-    screen.update()
+#TODO: Create a letter using starting_letter.txt
+with open(file="./Input/Letters/starting_letter.txt") as starting_letter:
+    starting_text = starting_letter.read()
+print(starting_text)
+with open(file="./Input/Names/invited_names.txt") as invited_friends:
+    friends = invited_friends.read()
+    list_of_friends = friends.split()
+for friend in list_of_friends:
+    new_letter = starting_text.replace("[name]", f"{friend}")
+    with open(file=f"./Output/ReadyToSend/{friend}_letter.txt", mode="w") as txt_letter:
+        txt_letter.write(new_letter)
 
 
-screen.exitonclick()
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
