4134149c6e32dc905c3fe98b0dc4c5c9b29fd8e8
Juan Barrera <jcbp1999@gmail.com>
2023-10-01 14:31:54 -0500

    birthday_mail_sender

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+import pandas
 import smtplib
 import datetime as dt
+import os
 import random
 
-my_email = "jcbp1999@gmail.com"
-password = "cwch ujti nmni lohz"
+# 1. Update the birthdays.csv
 
-day_of_week = dt.datetime.now().weekday()
+# DONE
 
-print(day_of_week)
+# 2. Check if today matches a birthday in the birthdays.csv
 
-if day_of_week == 6:
-    with open(file="quotes.txt", mode="r") as file:
-        quotes = file.readlines()
-        quote = random.choice(quotes)
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs=my_email,
-                            msg=f"Subject: Monday Motivation\n\n{quote}")
+BIRTHDAY_CSV_DIRECTORY = "birthdays.csv"
+LETTERS_DIRECTORY = "letter_templates/"
+MY_EMAIL = "jcbp1999@gmail.com"
+PASSWORD = "cwch ujti nmni lohz"
+
+letters = os.listdir(LETTERS_DIRECTORY)
+
+now = dt.datetime.now()
+now_day = now.day
+now_month = now.month
+birthday_data = pandas.read_csv(BIRTHDAY_CSV_DIRECTORY).to_dict(orient="records")
+for birthday in birthday_data:
+    if birthday["month"] == now_month and birthday["day"] == now_day:
+        letter = random.choice(letters)
+        with open(f"{LETTERS_DIRECTORY}{letter}", mode="r") as template:
+            birthday_template = template.read()
+        birthday_letter = birthday_template.replace("[NAME]", birthday["name"])
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=birthday["email"],
+                                msg=f"Subject: Happy birthday!\n\n{birthday_letter}")
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
