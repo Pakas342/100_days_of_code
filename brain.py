2b72160c13bcd2e8feb7a2f0217aa20ba1cd139d
Juan Barrera <jcbp1999@gmail.com>
2023-10-01 00:54:39 -0500

    flash_card_final_version

--- brain.py
+++ brain.py
@@ -2,20 +2,24 @@ import pandas
 import random as r
 
 # ---------------------------- CONSTANTS ------------------------------- #
-FILEPATH = "data/french_words.csv"
-
+FIRST_FILEPATH = "data/french_words.csv"
+SECOND_FILEPATH = "data/french_words_yet_to_know.csv"
 
 # ---------------------------- Functions SETUP ------------------------------- #
 
 
 class Brain:
     def __init__(self):
-        self.data = pandas.read_csv(FILEPATH).to_dict(orient="records")
+        try:
+            self.data = pandas.read_csv(SECOND_FILEPATH).to_dict(orient="records")
+        except FileNotFoundError:
+            self.data = pandas.read_csv(FIRST_FILEPATH).to_dict(orient="records")
+
         self.already_know = []
         self.words_to_choose = self.data
 
     def random_word(self):
-        if len(self.words_to_choose)  > 0:
+        if len(self.words_to_choose) > 0:
             return self.words_to_choose[r.randint(0, (len(self.words_to_choose) - 1))]
         else:
             pass
@@ -23,6 +27,8 @@ class Brain:
     def right_answer(self, word_dict):
         if len(self.words_to_choose) > 0:
             self.words_to_choose.remove(word_dict)
+            new_data = pandas.DataFrame(self.words_to_choose)
+            new_data.to_csv(SECOND_FILEPATH, index=False)
         else:
             pass
 
