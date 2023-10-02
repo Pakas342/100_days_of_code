dda186679cb088f5902a8afe4c7008da658afd29
Juan Barrera <jcbp1999@gmail.com>
2023-10-01 19:37:40 -0500

    iss_alert

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
-from tkinter import *
 import requests
+import datetime as dt
+import smtplib
+import time
 
-KANYE_URL = "https://api.kanye.rest"
-
-
-def get_quote():
-    response = requests.get(KANYE_URL)
-    quote = response.json()["quote"]
-    canvas.itemconfig(quote_text, text=quote)
-
-
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+SUNRISE_URL = "https://api.sunrise-sunset.org/json"
+MY_LAT = 4.6406589695490785
+MY_LNG = -74.06985854147935
+ISS_URL = "http://api.open-notify.org/iss-now.json"
+MY_EMAIL = "jcbp1999@gmail.com"
+PASSWORD = "cwch ujti nmni lohz"
 
 
+def iss_above(lat=MY_LAT, lng=MY_LNG):
+    iss_response = requests.get(url=ISS_URL)
+    iss_response.raise_for_status()
+    iss_lat = float(iss_response.json()["iss_position"]["latitude"])
+    iss_lng = float(iss_response.json()["iss_position"]["longitude"])
+    if abs(iss_lat - lat) < 5 and abs(iss_lng - lng) < 5:
+        return True
+    else:
+        return False
 
-window.mainloop()
+
+def is_night_time():
+
+    sunrise_parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
+
+    sunrise_response = requests.get(url=SUNRISE_URL, params=sunrise_parameters)
+    sunrise_response.raise_for_status()
+    time_data = sunrise_response.json()
+    sunrise_hour = int(time_data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset_hour = int(time_data["results"]["sunset"].split("T")[1].split(":")[0])
+    now_hour = int(dt.datetime.now().hour)
+    if now_hour >= sunset_hour or now_hour <= sunrise_hour:
+        return True
+    else:
+        return False
+
+
+while True:
+    time.sleep(60)
+    if iss_above() and is_night_time():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=MY_EMAIL,
+                                msg=f"Subject: ISS ABOVE!☝️\n\nLook up! you got the iss above")
+    else:
+        print("ISS no above")
